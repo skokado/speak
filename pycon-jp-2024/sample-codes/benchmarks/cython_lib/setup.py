90032328cc309f2e7156e8414825a5c663a9622c
skokado <terutacchi@gmail.com>
2024-10-01 23:26:04 +0900

    feat: build with openmp

--- pycon-jp-2024/sample-codes/benchmarks/cython_lib/setup.py
+++ pycon-jp-2024/sample-codes/benchmarks/cython_lib/setup.py
@@ -4,7 +4,23 @@ import sys
 from Cython.Build import cythonize
 
 
+if sys.platform.startswith("win"):
+    openmp_arg = '/openmp'
+else:
+    openmp_arg = '-fopenmp'
+
+
+ext_modules = [
+    Extension(
+        "*",
+        ["cython_lib.pyx"],
+        extra_compile_args=[openmp_arg],
+        extra_link_args=[openmp_arg],
+    )
+]
+
+
 setup(
     name='cython_lib',
-    ext_modules=cythonize("cython_lib.pyx"),
+    ext_modules=cythonize(ext_modules),
 )
